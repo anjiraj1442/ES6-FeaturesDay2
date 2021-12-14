19bbcb13b67606096af93719a05154f2fcde4024
anjiraj1442 <anjiraj1442@gmail.com>
2021-12-14 21:14:15 +0530

    Added:destructure is added

---
+++ ES6withexample/destructure.js
@@ -0,0 +1,12 @@
+const person ={
+     name:"Anjiraj",
+     age:24,
+     email:"anjirajardula@gmail.com"
+
+}
+//console.log(age)//undefined
+console.log(person.age)
+
+//destruture
+const {name,age,email}=person;
+console.log(email)
